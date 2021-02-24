ebd62b4c1e249f01266c5b098c1b4be82d8d0f4f
Patrick Genevich <patrickgenevich@gmail.com>
2021-02-24 09:01:05 -0800

    inital commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,74 +1,99 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, Bytes, ipfs, json, log } from "@graphprotocol/graph-ts";
 import {
   Contract,
   Approval,
   ApprovalForAll,
   ArtworkCreated,
   ArtworkSold,
-  Transfer
-} from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+  Transfer,
+  ArtworkPriceSet,
+} from "../generated/Contract/Contract";
+import { Artwork } from "../generated/schema";
 
-export function handleApproval(event: Approval): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+// Note: If a handler doesn't require existing field values, it is faster
+// _not_ to load the entity from the store. Instead, create it fresh with
+// `new Entity(...)`, set the fields that should be updated and save the
+// entity back to the store. Fields that were not set or unset remain
+// unchanged, allowing for partial updates to be applied.
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+// It is also possible to access smart contracts from mappings. For
+// example, the contract that has emitted the event can be connected to
+// with:
+//
+// let contract = Contract.bind(event.address)
+//
+// The following functions can then be called on this contract to access
+// state variables and other data:
+//
+// - contract.artworkToCurrentPrice(...)
+// - contract.balanceOf(...)
+// - contract.baseURI(...)
+// - contract.failedTransferCredits(...)
+// - contract.getApproved(...)
+// - contract.isApprovedForAll(...)
+// - contract.name(...)
+// - contract.ownerOf(...)
+// - contract.platform(...)
+// - contract.supportsInterface(...)
+// - contract.symbol(...)
+// - contract.tokenByIndex(...)
+// - contract.tokenOfOwnerByIndex(...)
+// - contract.tokenURI(...)
+// - contract.totalSupply(...)
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.artworkToCurrentPrice(...)
-  // - contract.balanceOf(...)
-  // - contract.baseURI(...)
-  // - contract.failedTransferCredits(...)
-  // - contract.getApproved(...)
-  // - contract.isApprovedForAll(...)
-  // - contract.name(...)
-  // - contract.ownerOf(...)
-  // - contract.platform(...)
-  // - contract.supportsInterface(...)
-  // - contract.symbol(...)
-  // - contract.tokenByIndex(...)
-  // - contract.tokenOfOwnerByIndex(...)
-  // - contract.tokenURI(...)
-  // - contract.totalSupply(...)
-}
+export function handleApproval(event: Approval): void {}
 
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
-export function handleArtworkCreated(event: ArtworkCreated): void {}
+export function handleArtworkCreated(event: ArtworkCreated): void {
+  let id = event.params.artworkId.toHex();
+  let artwork = Artwork.load(id);
 
-export function handleArtworkSold(event: ArtworkSold): void {}
+  if (artwork == null) {
+    artwork = new Artwork(id);
+  }
+
+  artwork.owner = event.params.artist;
+  artwork.artist = event.params.artist;
+
+  let contract = Contract.bind(event.address);
+  artwork.currentPrice = contract.artworkToCurrentPrice(event.params.artworkId);
+
+  let tokenURI = contract.tokenURI(event.params.artworkId);
+  log.info("Token uri {}", [tokenURI]);
+
+  let data = ipfs.cat(tokenURI);
+
+  if (data) {
+    let value = json.fromBytes(data as Bytes);
+    let obj = value.toObject();
+    artwork.name = obj.getEntry("name").value.toString();
+    artwork.description = obj.getEntry("description").value.toString();
+    artwork.image = obj.getEntry("image").value.toString();
+  } else {
+    log.error("Error no data for token URI {}", [tokenURI]);
+  }
+
+  artwork.save();
+}
+
+export function handleArtworkSold(event: ArtworkSold): void {
+  /*
+  let id = event.params.artworkId.toHex();
+  let artwork = Artwork.load(id);
+  artwork.owner = event.params.newOwner;
+  artwork.soldPriceHistory.push(event.params.price);
+  artwork.save();
+  */
+}
+
+export function handleArtworkPriceSet(event: ArtworkPriceSet): void {
+  /*
+  let id = event.params.artworkId.toHex();
+  let artwork = Artwork.load(id);
+  artwork.currentPrice = event.params.price;
+  artwork.save();
+  */
+}
 
 export function handleTransfer(event: Transfer): void {}
